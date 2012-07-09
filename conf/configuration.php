16adb949ffe06460b5c2970748e433e07c0b9950
Nicolas Le Goff <legoff.n@gmail.com>
2012-07-09 11:42:24 +0200

    add conf

--- conf/configuration.php
+++ conf/configuration.php
@@ -2,10 +2,10 @@
 
 
 return array(
-    'dev-token' => '5e2dde28284f498ed2088b1c08910619',
-    'instance-url' => 'http://dev.phrasea.net/',
-    'api-key' => '2188489d00339339ebea969cddafc317',
-    'api-secret' => '361fdc9a27d507adec3cb29b34100f8b',
-    "callback-uri" =>  'http://localhost/Phraseanet-PHP-SDK-Starter-Kit/www/',
-    "authentication-uri" =>  'http://localhost/Phraseanet-PHP-SDK-Starter-Kit/www/',
+    'dev-token' => '',
+    'instance-url' => '',
+    'api-key' => '',
+    'api-secret' => '',
+    "callback-uri" =>  '',
+    "authentication-uri" =>  '',
 );
